934916a91213f1ae196dfd9c333a9f1e0d4c842c
AlessioGr <70709113+AlessioGr@users.noreply.github.com>
2021-09-27 14:41:32 +0200

    Fix console colors on Spigot

--- src/main/java/notquests/notquests/Managers/LogManager.java
+++ src/main/java/notquests/notquests/Managers/LogManager.java
@@ -13,22 +13,27 @@ public class LogManager {
     private final Audience consoleSender;
     private final Component prefix;
     private final String prefixText;
+    private boolean isPaper = false;
 
 
     public LogManager(final NotQuests main) {
         this.main = main;
         consoleSender = main.adventure().sender(Bukkit.getConsoleSender());
 
-        prefixText = "<#393e46>[<gradient:#E0EAFC:#CFDEF3>NotQuests<#393e46>]<#636c73>: ";
-        prefix = MiniMessage.miniMessage().parse(prefixText);
 
-
-        boolean isPaper = false;
         try {
-            // Any other works, just the shortest I could find.
+            // Paper-only method
             Class.forName("com.destroystokyo.paper.ParticleBuilder");
             isPaper = true;
         } catch (ClassNotFoundException ignored) {
+        }
+
+        if (isPaper) {
+            prefixText = "<#393e46>[<gradient:#E0EAFC:#CFDEF3>NotQuests<#393e46>]<#636c73>: ";
+            prefix = MiniMessage.miniMessage().parse(prefixText);
+        } else {
+            prefixText = "<DARK_GRAY>[<WHITE>NotQuests<DARK_GRAY>]<GRAY>: ";
+            prefix = MiniMessage.miniMessage().parse(prefixText);
         }
     }
 
@@ -43,7 +48,12 @@ public class LogManager {
 
         String afterPrefixSuffix = "<WHITE>";
         if (level == Level.INFO) {
-            afterPrefixSuffix = "<gradient:#37a659:#56B4D3>";
+            if (isPaper) {
+                afterPrefixSuffix = "<gradient:#37a659:#56B4D3>";
+            } else {
+                afterPrefixSuffix = "<GREEN>";
+            }
+
         } else if (level == Level.WARNING) {
             afterPrefixSuffix = "<YELLOW>";
         } else if (level == Level.SEVERE) {
@@ -59,11 +69,29 @@ public class LogManager {
         String afterPrefixSuffix = "<WHITE>";
         if (level == Level.INFO) {
             if (logCategory == LogCategory.DEFAULT) {
-                afterPrefixSuffix = "<gradient:#37a659:#56B4D3>";
+                if (isPaper) {
+                    afterPrefixSuffix = "<gradient:#37a659:#56B4D3>";
+
+                } else {
+                    afterPrefixSuffix = "<GREEN>";
+                }
+
             } else if (logCategory == LogCategory.DATA) {
-                afterPrefixSuffix = "<gradient:#1FA2FF:#12D8FA:#A6FFCB>";
+                if (isPaper) {
+                    afterPrefixSuffix = "<gradient:#1FA2FF:#12D8FA:#A6FFCB>";
+
+                } else {
+                    afterPrefixSuffix = "<BLUE>";
+                }
+
             } else if (logCategory == LogCategory.LANGUAGE) {
-                afterPrefixSuffix = "<gradient:#AA076B:#61045F>";
+                if (isPaper) {
+                    afterPrefixSuffix = "<gradient:#AA076B:#61045F>";
+
+                } else {
+                    afterPrefixSuffix = "<DARK_PURPLE>";
+                }
+
             }
 
         } else if (level == Level.WARNING) {
