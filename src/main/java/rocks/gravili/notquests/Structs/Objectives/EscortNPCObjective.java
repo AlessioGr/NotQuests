1c070d138cb14822e31d20e71390c97125ca8773
AlessioGr <alessio@gravili.de>
2021-11-26 22:31:26 +0100

    Move EscortNPCObjective to new command system
    Signed-off-by: AlessioGr <alessio@gravili.de>

--- src/main/java/rocks/gravili/notquests/Structs/Objectives/EscortNPCObjective.java
+++ src/main/java/rocks/gravili/notquests/Structs/Objectives/EscortNPCObjective.java
@@ -18,14 +18,23 @@
 
 package rocks.gravili.notquests.Structs.Objectives;
 
+import cloud.commandframework.ArgumentDescription;
 import cloud.commandframework.Command;
+import cloud.commandframework.arguments.standard.IntegerArgument;
+import cloud.commandframework.meta.CommandMeta;
 import cloud.commandframework.paper.PaperCommandManager;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import rocks.gravili.notquests.Commands.NotQuestColors;
 import rocks.gravili.notquests.NotQuests;
 import rocks.gravili.notquests.Structs.Quest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EscortNPCObjective extends Objective {
 
@@ -86,7 +95,58 @@ public class EscortNPCObjective extends Objective {
     }
 
 
-    public static void handleCommands(NotQuests main, PaperCommandManager<CommandSender> manager, Command.Builder<CommandSender> builder) {
+    public static void handleCommands(NotQuests main, PaperCommandManager<CommandSender> manager, Command.Builder<CommandSender> addObjectiveBuilder) {
+        manager.command(addObjectiveBuilder.literal("EscortNPC")
+                .argument(IntegerArgument.<CommandSender>newBuilder("NPC to escort").withSuggestionsProvider((context, lastString) -> {
+                    ArrayList<String> completions = new ArrayList<>();
+                    for (final NPC npc : CitizensAPI.getNPCRegistry().sorted()) {
+                        completions.add("" + npc.getId());
+                    }
+                    final List<String> allArgs = context.getRawInput();
+                    final Audience audience = main.adventure().sender(context.getSender());
+                    main.getUtilManager().sendFancyCommandCompletion(audience, allArgs.toArray(new String[0]), "[NPC to escort ID]", "[Destination NPC ID]");
 
+                    return completions;
+                }).build(), ArgumentDescription.of("ID of the Citizens NPC the player has to escort."))
+                .argument(IntegerArgument.<CommandSender>newBuilder("Destination NPC").withSuggestionsProvider((context, lastString) -> {
+                    ArrayList<String> completions = new ArrayList<>();
+                    for (final NPC npc : CitizensAPI.getNPCRegistry().sorted()) {
+                        if (!("" + npc.getId()).equalsIgnoreCase(context.get("NPC to escort"))) {
+                            completions.add("" + npc.getId());
+                        }
+                    }
+                    final List<String> allArgs = context.getRawInput();
+                    final Audience audience = main.adventure().sender(context.getSender());
+                    main.getUtilManager().sendFancyCommandCompletion(audience, allArgs.toArray(new String[0]), "[Destination NPC ID]", "");
+
+                    return completions;
+                }).build(), ArgumentDescription.of("ID of the destination Citizens NPC where the player has to escort the NPC to escort to."))
+                .meta(CommandMeta.DESCRIPTION, "Adds a new EscortNPC Objective to a quest.")
+                .handler((context) -> {
+                    final Audience audience = main.adventure().sender(context.getSender());
+                    final Quest quest = context.get("quest");
+
+                    final int toEscortNPCID = context.get("NPC to escort");
+                    final int destinationNPCID = context.get("Destination NPC");
+
+                    if (toEscortNPCID == destinationNPCID) {
+                        audience.sendMessage(
+                                MiniMessage.miniMessage().parse(
+                                        NotQuestColors.errorGradient + "Error: Um... an NPC cannot themselves himself, to.. themselves?"
+                                )
+                        );
+                        return;
+                    }
+
+                    EscortNPCObjective escortNPCObjective = new EscortNPCObjective(main, quest, quest.getObjectives().size() + 1, toEscortNPCID, destinationNPCID);
+                    quest.addObjective(escortNPCObjective, true);
+
+                    audience.sendMessage(MiniMessage.miniMessage().parse(
+                            NotQuestColors.successGradient + "EscortNPC Objective successfully added to Quest " + NotQuestColors.highlightGradient
+                                    + quest.getQuestName() + "</gradient>!</gradient>"
+                    ));
+
+
+                }));
     }
 }
