a0eeb541f659160625bdff9074dfe0ab17eebc3f
AlessioGr <alessio@gravili.de>
2021-11-27 03:47:32 +0100

    Move WORLDENTER Trigger to new command system
    Signed-off-by: AlessioGr <alessio@gravili.de>

--- src/main/java/rocks/gravili/notquests/Structs/Triggers/TriggerTypes/WorldEnterTrigger.java
+++ src/main/java/rocks/gravili/notquests/Structs/Triggers/TriggerTypes/WorldEnterTrigger.java
@@ -18,13 +18,25 @@
 
 package rocks.gravili.notquests.Structs.Triggers.TriggerTypes;
 
+import cloud.commandframework.ArgumentDescription;
 import cloud.commandframework.Command;
+import cloud.commandframework.arguments.standard.IntegerArgument;
+import cloud.commandframework.arguments.standard.StringArgument;
+import cloud.commandframework.meta.CommandMeta;
 import cloud.commandframework.paper.PaperCommandManager;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import rocks.gravili.notquests.Commands.NotQuestColors;
 import rocks.gravili.notquests.NotQuests;
 import rocks.gravili.notquests.Structs.Quest;
 import rocks.gravili.notquests.Structs.Triggers.Action;
 import rocks.gravili.notquests.Structs.Triggers.Trigger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorldEnterTrigger extends Trigger {
 
@@ -60,6 +72,51 @@ public class WorldEnterTrigger extends Trigger {
     }
 
     public static void handleCommands(NotQuests main, PaperCommandManager<CommandSender> manager, Command.Builder<CommandSender> addTriggerBuilder) {
+        manager.command(addTriggerBuilder.literal("WORLDENTER")
+                .argument(StringArgument.<CommandSender>newBuilder("world to enter").withSuggestionsProvider(
+                        (context, lastString) -> {
+                            final List<String> allArgs = context.getRawInput();
+                            final Audience audience = main.adventure().sender(context.getSender());
+                            main.getUtilManager().sendFancyCommandCompletion(audience, allArgs.toArray(new String[0]), "[World Name / 'ALL']", "[Amount of Enters]");
 
+                            ArrayList<String> completions = new ArrayList<>();
+
+                            completions.add("ALL");
+
+                            for (final World world : Bukkit.getWorlds()) {
+                                completions.add(world.getName());
+                            }
+
+                            return completions;
+                        }
+                ).single().build(), ArgumentDescription.of("Name of the world which needs to be entered"))
+                .argument(IntegerArgument.<CommandSender>newBuilder("amount").withMin(1), ArgumentDescription.of("Amount of times the world needs to be entered."))
+                .flag(main.getCommandManager().applyOn)
+                .flag(main.getCommandManager().triggerWorldString)
+                .meta(CommandMeta.DESCRIPTION, "Triggers when the player enters a specific world.")
+                .handler((context) -> {
+                    final Audience audience = main.adventure().sender(context.getSender());
+
+                    final Quest quest = context.get("quest");
+                    final Action action = context.get("action");
+
+                    final String worldToEnterName = context.get("world to enter");
+
+                    int amountOfWorldEnters = context.get("amount");
+
+                    final int applyOn = context.flags().getValue(main.getCommandManager().applyOn, 0); //0 = Quest
+                    final String worldString = context.flags().getValue(main.getCommandManager().triggerWorldString, null);
+
+
+                    WorldEnterTrigger worldEnterTrigger = new WorldEnterTrigger(main, quest, quest.getTriggers().size() + 1, action, applyOn, worldString, amountOfWorldEnters, worldToEnterName);
+
+                    quest.addTrigger(worldEnterTrigger);
+
+                    audience.sendMessage(MiniMessage.miniMessage().parse(
+                            NotQuestColors.successGradient + "WORLDENTER Trigger successfully added to Quest " + NotQuestColors.highlightGradient
+                                    + quest.getQuestName() + "</gradient>!</gradient>"
+                    ));
+
+                }));
     }
 }
