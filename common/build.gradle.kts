f41d6af65ca6c14d5b1a251cbe17d432aaa08496
AlessioGr <alessio@gravili.de>
2021-12-26 17:53:52 +0100

    Update to v3.1.1
    Signed-off-by: AlessioGr <alessio@gravili.de>

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "rocks.gravili.notquests"
-version = "3.1.0"
+version = "3.1.1"
 
 repositories {
     mavenCentral()
@@ -149,7 +149,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "rocks.gravili.notquests"
             artifactId = "NotQuests"
-            version = "3.1.0"
+            version = "3.1.1"
 
             from(components["java"])
         }
