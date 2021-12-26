1a3d9f9a7e32687e212599ddf0cfdce0a7405f5c
AlessioGr <alessio@gravili.de>
2021-12-27 00:23:48 +0100

    Update to v3.2.0
    Signed-off-by: AlessioGr <alessio@gravili.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 
 group = "rocks.gravili.notquests"
-version = "3.1.1"
+version = "3.2.0"
 
 repositories {
 }
@@ -47,7 +47,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "rocks.gravili.notquests"
             artifactId = "NotQuests"
-            version = "3.1.1"
+            version = "3.2.0"
 
             from(components["java"])
         }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "rocks.gravili.notquests"
-version = "3.1.1"
+version = "3.2.0"
 
 repositories {
     mavenCentral()
@@ -156,7 +156,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "rocks.gravili.notquests"
             artifactId = "NotQuests"
-            version = "3.1.1"
+            version = "3.2.0"
 
             from(components["java"])
         }
