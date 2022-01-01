c2d59afa251b69763e5804f3c4469f2a74b298eb
AlessioGr <alessio@gravili.de>
2022-01-01 02:54:03 +0100

    github actions packages
    Signed-off-by: AlessioGr <alessio@gravili.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/AlessioGr/NotQuests")
+            url = uri("https://maven.pkg.github.com/alessiogr/NotQuests")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("PASSWORD")
