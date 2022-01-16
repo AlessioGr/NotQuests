94216ff8aa9d573003f97c02a0b934b3856077dd
AlessioGr <alessio@gravili.de>
2022-01-16 11:12:28 +0100

    Web Editor
    Signed-off-by: AlessioGr <alessio@gravili.de>

--- paper/src/main/java/rocks/gravili/notquests/paper/managers/WebManager.java
+++ paper/src/main/java/rocks/gravili/notquests/paper/managers/WebManager.java
@@ -17,12 +17,14 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.http.client.HttpClient;
+import rocks.gravili.notquests.paper.managers.data.Category;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class WebManager {
     private final NotQuests main;
+    Gson gson          = new Gson();
 
     public WebManager(final NotQuests main){
         this.main = main;
@@ -45,7 +47,6 @@ public class WebManager {
     public String sendRequest(JsonObject jsonObject) throws IOException {
         //String       postURL       = "www.notquests.com/webeditornew";// put in your url
         String postURL = "https://notquestsprisma.vercel.app/api/webeditors";
-        Gson gson          = new Gson();
         HttpClient   httpClient    = HttpClientBuilder.create().build();
         HttpPost post          = new HttpPost(postURL);
         StringEntity postingString = new StringEntity(gson.toJson(jsonObject));//gson.tojson() converts your pojo to json
@@ -56,29 +57,15 @@ public class WebManager {
 
         HttpResponse result = httpClient.execute(post);
 
-        String json = EntityUtils.toString(result.getEntity(), "UTF-8");
 
-
-        return json;
+        return EntityUtils.toString(result.getEntity(), "UTF-8");
 
 
         //main.getLogManager().info("Response: " + response.toString());
     }
 
     public String openEditor() {
-        String jsonDefaultCategoryQuests = "{}";
-
-        try{
-            jsonDefaultCategoryQuests = convertYamlToJson(main.getDataManager().getDefaultCategory().getQuestsConfig().saveToString());
-        }catch (JsonProcessingException e){
-            main.getLogManager().warn("Cannot convert Quests YAML to web-ready JSON.");
-            e.printStackTrace();
-        }
-
-        main.getLogManager().info("JSON to send: \n" + jsonDefaultCategoryQuests);
-
-
-        JsonObject jsonObject = JsonParser.parseString(jsonDefaultCategoryQuests).getAsJsonObject();
+        JsonObject jsonObject = toJson();
 
         try{
             String jsonReturnObject = sendRequest(jsonObject);
@@ -92,4 +79,60 @@ public class WebManager {
 
         return "Cannot send web request!";
     }
+
+    public JsonObject toJson(){
+        /*String jsonDefaultCategoryQuests = "{}";
+
+        try{
+            jsonDefaultCategoryQuests = convertYamlToJson(main.getDataManager().getDefaultCategory().getQuestsConfig().saveToString());
+        }catch (JsonProcessingException e){
+            main.getLogManager().warn("Cannot convert Quests YAML to web-ready JSON.");
+            e.printStackTrace();
+        }
+
+        main.getLogManager().info("JSON to send: \n" + jsonDefaultCategoryQuests);*/
+
+
+
+        JsonObject categories = new JsonObject();
+
+        try{
+            for(Category category : main.getDataManager().getCategories()){
+                JsonObject categoryObject = new JsonObject();
+
+                String ymlQuestsString = category.getQuestsConfig().saveToString();
+                String ymlActionsString = category.getActionsConfig().saveToString();
+                String ymlConditionsString = category.getConditionsConfig().saveToString();
+
+                if(!ymlQuestsString.isBlank()){
+                    String jsonQuestsString = convertYamlToJson(ymlQuestsString);
+                    categoryObject.add("Quests",  JsonParser.parseString(jsonQuestsString));
+                }
+                if(!ymlActionsString.isBlank()){
+                    String jsonActionsString = convertYamlToJson(ymlActionsString);
+                    categoryObject.add("Actions", JsonParser.parseString(jsonActionsString));
+                }
+                if(!ymlConditionsString.isBlank()){
+                    String jsonConditionsString = convertYamlToJson(ymlConditionsString);
+                    categoryObject.add("Conditions", JsonParser.parseString(jsonConditionsString));
+                }
+
+
+
+
+
+                categories.add(category.getCategoryName(), categoryObject);
+
+            }
+        }catch (Exception e){
+            main.getLogManager().warn("Cannot convert Quests YAML to web-ready JSON.");
+            e.printStackTrace();
+        }
+
+
+
+        return categories;
+
+    }
+
 }
