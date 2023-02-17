8935b7af93600bfec00193849e278a3188502ea6
Jakob Reiser <jakobreiser@gmx.de>
2023-02-17 23:58:24 +0100

    Fix armor being visible in armor slots after completing DeliverItems objective

--- paper/src/main/java/rocks/gravili/notquests/paper/events/hooks/CitizensEvents.java
+++ paper/src/main/java/rocks/gravili/notquests/paper/events/hooks/CitizensEvents.java
@@ -186,7 +186,7 @@ public class CitizensEvents implements Listener {
                         }
 
                     }
-
+                    player.updateInventory();
                 }
             }
         });
