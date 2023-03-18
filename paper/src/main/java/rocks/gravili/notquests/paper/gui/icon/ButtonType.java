27c5204990fc751792d0d5107e9aa49bd770c1e7
Jakob Reiser <jakobreiser@gmx.de>
2023-03-18 11:06:28 +0100

    Add missing files for gui feature because somehow they were ignored?

---
+++ paper/src/main/java/rocks/gravili/notquests/paper/gui/icon/ButtonType.java
@@ -0,0 +1,13 @@
+package rocks.gravili.notquests.paper.gui.icon;
+
+public enum ButtonType {
+    BLANK,
+    PAGED,
+    TAB,
+    ACTION,
+    BACK,
+    UP,
+    DOWN,
+    FORWARD
+
+}
---
+++ paper/src/main/java/rocks/gravili/notquests/paper/gui/icon/SpecialIconType.java
@@ -0,0 +1,10 @@
+package rocks.gravili.notquests.paper.gui.icon;
+
+public enum SpecialIconType {
+    DEFAULT,
+    PLAYER_ACTIVE_QUEST,
+    PLAYER_AVAILABLE_QUEST,
+    CATEGORY,
+    CATEGORY_AVAILABLE_QUEST,
+    NPC_SHOWN_QUEST
+}
