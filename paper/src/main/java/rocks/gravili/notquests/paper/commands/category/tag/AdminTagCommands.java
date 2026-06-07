a4a60ee801e90d8f3c50286cd89a2ed44a461de4
Alessio Gravili <github@gravili.net>
2026-06-07 02:36:33 +0000

    feat: add /qa tags check command

--- paper/src/main/java/rocks/gravili/notquests/paper/commands/category/tag/AdminTagCommands.java
+++ paper/src/main/java/rocks/gravili/notquests/paper/commands/category/tag/AdminTagCommands.java
@@ -19,9 +19,11 @@
 package rocks.gravili.notquests.paper.commands.category.tag;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.incendo.cloud.Command;
 import org.incendo.cloud.description.Description;
 import org.incendo.cloud.paper.LegacyPaperCommandManager;
+import org.incendo.cloud.parser.flag.CommandFlag;
 import org.incendo.cloud.suggestion.Suggestion;
 import rocks.gravili.notquests.paper.NotQuests;
 import rocks.gravili.notquests.paper.managers.data.Category;
@@ -30,6 +32,7 @@ import rocks.gravili.notquests.paper.managers.tags.TagType;
 
 import java.util.concurrent.CompletableFuture;
 
+import static org.incendo.cloud.bukkit.parser.PlayerParser.playerParser;
 import static org.incendo.cloud.parser.standard.EnumParser.enumParser;
 import static org.incendo.cloud.parser.standard.StringParser.stringParser;
 
@@ -122,6 +125,57 @@ public class AdminTagCommands {
                             "<success>The tag <highlight>"
                                     + tagName
                                     + "</highlight> has been deleted successfully!")
+                    );
+                }));
+
+        final CommandFlag<Player> tagCheckPlayerFlag =
+                CommandFlag.builder("player").withComponent(playerParser()).build();
+
+        manager.command(editBuilder.commandDescription(Description.of("Shows a player's current value for a tag."))
+                .literal("check")
+                .required("tag-name", stringParser(), (context, input) -> {
+                    main.getUtilManager().sendFancyCommandCompletion(
+                            context.sender(),
+                            input.input().split(" "),
+                            "[Tag Name]",
+                            "");
+                    return CompletableFuture.completedFuture(main.getTagManager().getTags().stream().map(tag -> Suggestion.suggestion(tag.getTagName())).toList());
+                })
+                .flag(tagCheckPlayerFlag)
+                .handler((context) -> {
+                    final String tagName = context.get("tag-name");
+
+                    final Tag foundTag = main.getTagManager().getTag(tagName);
+                    if (foundTag == null) {
+                        context.sender().sendMessage(main.parse(
+                                "<error>Error: The tag <highlight>"
+                                        + tagName
+                                        + "</highlight> doesn't exists!")
+                        );
+                        return;
+                    }
+
+                    final Player playerSelector = context.flags().getValue(tagCheckPlayerFlag, null);
+                    final Player player;
+                    if (playerSelector != null) {
+                        player = playerSelector;
+                    } else if (context.sender() instanceof final Player senderPlayer) {
+                        player = senderPlayer;
+                    } else {
+                        context.sender().sendMessage(main.parse(
+                                "<error>Error: Run this in-game, or specify a player with <highlight>--player</highlight> from console.")
+                        );
+                        return;
+                    }
+
+                    final Object tagValue = main.getQuestPlayerManager()
+                            .getOrCreateQuestPlayer(player.getUniqueId())
+                            .getTagValue(foundTag.getTagName());
+
+                    context.sender().sendMessage(main.parse(
+                            "<main>" + foundTag.getTagType().name().toLowerCase() + " tag <highlight>"
+                                    + foundTag.getTagName() + "</highlight> for <highlight2>" + player.getName()
+                                    + "</highlight2>:</main> <highlight>" + (tagValue != null ? tagValue : "not set"))
                     );
                 }));
     }
